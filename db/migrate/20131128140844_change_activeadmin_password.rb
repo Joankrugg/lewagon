94ea3a9461e3f36152affde7ca73ebf6c29aa371
Boris Paillard <boris.paillard@gmail.com>
2013-11-28 15:20:52 +0100

    delete first admin user

--- db/migrate/20131128140844_change_activeadmin_password.rb
+++ db/migrate/20131128140844_change_activeadmin_password.rb
@@ -1,10 +1,12 @@
 class ChangeActiveadminPassword < ActiveRecord::Migration
   def up
+    AdminUser.find_by_email('admin@example.com').destroy
     AdminUser.create!(:email => 'contact@lewagon.org', :password => 'lewagon0611', :password_confirmation => 'lewagon0611') 
   end
 
   def down
     AdminUser.find_by_email('contact@lewagon.org').destroy
+    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') 
   end
 
 end
