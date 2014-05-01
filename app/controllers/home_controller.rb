eee1c2a7250a3d35295759b777965c0be3f1151d
Sebastien Saunier <seb@saunier.me>
2014-05-01 15:52:30 +0200

    Prepare /postulez page to receive wufoo forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  
+
   def index
   	sign_out :user
     render 'page', page: "home"
@@ -8,16 +8,16 @@ class HomeController < ApplicationController
   def merci
 	sign_out :user
   end
-  
+
   def page
     @next_course = Course.where("date >= :today", today: Date.today).order("date ASC").limit(2) if params[:page] == "home"
     render params[:page]
   end
-  
+
   def subscribe
     email = params[:email][:email]
     gb = Gibbon::API.new
-    @response = gb.lists.subscribe({:id => 'b90ec0618e', :email => {:email => email}, :double_optin => false})    
+    @response = gb.lists.subscribe({:id => 'b90ec0618e', :email => {:email => email}, :double_optin => false})
   end
-  
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def postulate(text, options = {})
+    # link_to text, { controller: "registrations", action: "new" }, options
+    link_to text, "/postuler", options
+  end
 end
